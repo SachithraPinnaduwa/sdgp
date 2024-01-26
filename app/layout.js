39b9eb3c414bc42fa36c89867eadf6d727a187ba
Oshani Kavindya Halgamuwa <oshani.20221137@iit.ac.lk>
2024-01-26 10:02:05 +0530

    change

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-    <body className={"bg-blue-900 min-h-screen "}>
+    <body className={"bg-gradient-radial min-h-screen "}>
 
 
       {children}
