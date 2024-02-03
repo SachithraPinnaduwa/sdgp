6aace4313faf8db69c6a01ba1f3dc3e126a47095
Sachithra Pinnaduwa <136422527+SachithraPinnaduwa@users.noreply.github.com>
2024-02-03 12:42:55 +0530

    posting user data

--- backend/controllers/logincontroller.js
+++ backend/controllers/logincontroller.js
@@ -1,5 +1,11 @@
 const axios = require("axios");
 
+const users = [
+  {name: "John Doe", email: "sachithra@gmail.com", id: 1, scam: "scam"},
+{name: "Jane Doe", email: "jane@gmail.com", id: 2, scam: "scam2"},
+{name: "John Smith", email: "john@gmail.com", id: 3, scam: "scam3"},
+]; 
+
 async function GetCart(req, res) {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
@@ -11,7 +17,14 @@ async function GetCart(req, res) {
   }
 }
 function postUserData(req,res) {
-  res.status(200).send({ id: req.body.id, name: req.body.name, email: req.body.email,scam: req.body.scam});
-  
+  const parseID = parseInt(req.body.id);
+  const{body} = req;
+  const newUser = {id:users[users.length-1].id+1,...body};
+  users.push(newUser)
+  // if (isNaN(parseID)) {
+  //   return res.status(400).send({ error: "Invalid ID" });
+  // }
+  // res.status(200).send({  id: req.body.id, name: req.body.name, email: req?.body.email,scam: req?.body.scam});
+  return res.status(200).send(users);
 }
 module.exports = { GetCart, postUserData };
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const login = require("../controllers/logincontroller.js");
-router.route("/").get(login.GetCart).post(login.postUserData);
+const {query,body, validationResult} = require('express-validator')
+router.route("/").get(login.GetCart).post(body("name").isString().notEmpty().withMessage("username cannot be empty"),(req,res,next)=>{
+    //this is middleware used for validation
+    const result = validationResult(req)
+    console.log(result)
+    if (result.isEmpty()) {
+       
+        next()
+    }
+    res.send("validation incorrect")
+},login.postUserData);
 
 module.exports = router;
