42baf68a503616698d656279fc7b1ae49b180b27
Sachithra Pinnaduwa <136422527+SachithraPinnaduwa@users.noreply.github.com>
2024-02-05 21:38:46 +0530

    connected to the online

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-
+const {connectToDatabase} = require('./db/connection'); 
 const express = require('express')
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
 app.use(express.json());
-const mongoose = require('mongoose');
+
 app.use(cors());
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("connected to database"))
+connectToDatabase()
+.then(()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+})
+console.log("connected to database")
+})
 .catch((err)=>console.log(err))
 const login = require("./routes/login");
 
@@ -17,7 +23,3 @@ app.use('/login',login);
 app.get('/',(req,res)=>{
     res.send({message:"main page of server"})
 })
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
