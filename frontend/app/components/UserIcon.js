abc65360cd54607ef3bb8bc7c83d497c500a761f
Oshani Kavindya Halgamuwa <oshani.20221137@iit.ac.lk>
2024-02-13 14:54:19 +0530

    Update UserIcon.js

--- frontend/app/components/UserIcon.js
+++ frontend/app/components/UserIcon.js
@@ -58,6 +58,9 @@ const UserIcon = () => {
                 <li>
                   <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
                 </li>
+                <li>
+                  <a href="/Signin" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign in</a>
+                </li>
               </ul>
             </div>
           </div>
