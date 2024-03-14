d5f135dd25d31ede0486565f7aa36fa74b68d132
GayanaOW <125947588+GayanaOW@users.noreply.github.com>
2024-03-15 00:16:16 +0530

    Update AuthContext.js

--- frontend/app/context/AuthContext.js
+++ frontend/app/context/AuthContext.js
@@ -72,8 +72,8 @@ const data = await loginUser(email, password);
     return data;
   };
 
-  const updateUserDetails = async (name,email,password,firstName,lastName) => {
-    const data = await updateUser(name,email,password,firstName,lastName)
+  const updateUserDetails = async (name,email,password,firstName,lastName,postImage) => {
+    const data = await updateUser(name,email,password,firstName,lastName,postImage)
     return data;
   };
   
