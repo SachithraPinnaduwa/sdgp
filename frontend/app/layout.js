be520e7c7ddce7ea273f555c1f20d50f41972c3c
Sachithra Pinnaduwa <136422527+SachithraPinnaduwa@users.noreply.github.com>
2024-03-23 10:17:42 +0530

    added vercel insights

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,7 +4,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
-
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +24,8 @@ export default function RootLayout({ children }) {
     <Toaster position="top-center"  />
    
       {children}
-
-    
+      <Analytics />
+      <SpeedInsights />
     </body>
     </AuthProvider>
     </html>
