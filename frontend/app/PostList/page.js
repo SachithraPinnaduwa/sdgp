6fb269242567c187c9342bb292f54d8cb32b139f
Sachithra Pinnaduwa <136422527+SachithraPinnaduwa@users.noreply.github.com>
2024-03-24 23:51:29 +0530

    updated the search

--- frontend/app/PostList/page.js
+++ frontend/app/PostList/page.js
@@ -55,7 +55,7 @@ const PostList = () => {
                 </div>
                 <input type="search" value={searchQuery}
                   onChange={handleSearch} id="default-search"
-                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for a specific post" required />
               </div>
             </form>
           </div>
